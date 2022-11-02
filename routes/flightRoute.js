5b14a83eff856981a3f4b8585e7d812cef278b4a
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-02 09:07:02 +0100

    add route to add a new flight record

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+//router.get('/', controller.example);
+
+// This route adds a new flight record to the database
+router.post('/add', controller.addFlight);
 
 module.exports = router;
-
