6e465391b387f4c95b1309c11abfeb31e3dae5a3
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-02 09:34:43 +0100

    add route to fetch all flights

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-//router.get('/', controller.example);
+// returns all flights data
+router.get('/', controller.allFlights);
 
 // This route adds a new flight record to the database
 router.post('/add', controller.addFlight);
