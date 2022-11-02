b8444b046c8a02f4dacf6c71e95cb869c7e680cf
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-02 10:02:44 +0100

    create route to get single flight data using its ID

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -9,4 +9,7 @@ router.get('/', controller.allFlights);
 // This route adds a new flight record to the database
 router.post('/add', controller.addFlight);
 
+// Get single flight data using its ID
+router.get('/:id', controller.getFlight);
+
 module.exports = router;
