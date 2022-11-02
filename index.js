86d32f9d943ba5af68fbabee84e997f034809bc4
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-02 09:02:11 +0100

    change the PORT of the server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(json());
 
 app.use("/", routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
